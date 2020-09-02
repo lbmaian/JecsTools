684c8cb86028321d55d92035eadb30e73c472f3b
lbmaian <maian330@gmail.com>
2020-09-02 00:37:53 -0700

    Improve CompShield performance by avoiding generic GetComp<T> and LINQ usage
    ThingWithComps.GetComp<T> is slow because the `isinst` instruction with generic type arg operands is very slow.

--- Source/AllModdingComponents/PawnShields/Utility/ShieldUtility.cs
+++ Source/AllModdingComponents/PawnShields/Utility/ShieldUtility.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using Verse;
+﻿using Verse;
 
 namespace PawnShields
 {
@@ -8,6 +7,17 @@ namespace PawnShields
     /// </summary>
     public static class ShieldUtility
     {
+        public static CompShield GetCompShield(this ThingWithComps thing)
+        {
+            var comps = thing.AllComps;
+            for (int i = 0, count = comps.Count; i < count; i++)
+            {
+                if (comps[i] is CompShield comp)
+                    return comp;
+            }
+            return null;
+        }
+
         /// <summary>
         /// Attempts to get the first shield from the pawn.
         /// </summary>
@@ -27,7 +37,14 @@ namespace PawnShields
         /// <returns>Shield if tracker has any or null if there is no shield.</returns>
         public static ThingWithComps GetShield(this Pawn_EquipmentTracker eqTracker)
         {
-            return eqTracker.AllEquipmentListForReading.FirstOrDefault(thing => thing.GetComp<CompShield>() != null);
+            var allEquipment = eqTracker.AllEquipmentListForReading;
+            for (int i = 0, count = allEquipment.Count; i < count; i++)
+            {
+                var equipment = allEquipment[i];
+                if (equipment.GetCompShield() != null)
+                    return equipment;
+            }
+            return null;
         }
     }
 }
